8bb4ebf442855560e59bc79e99c5f2cb845dee4b
jjang051 <jjang051@hanmail.net>
2021-12-13 10:49:45 +0900

    함수 만들고 gnb 점찍기 완성

--- js/main.js
+++ js/main.js
@@ -1,51 +1,45 @@
 const main = $("#main");
-
-let fp = null;
+let fullPage = null;
+//함수 선언  hoisting
+loadJson("../data/bigbang.json");
 function loadJson(jsonFile) {
-  let total = 0;
   $.ajax({
     url: jsonFile,
     success: function (res) {
-      console.log(res);
       const clockList = res.clock;
-      //forEach(function(item,idx){})
-      total = clockList.length;
       let output = "";
       $.each(clockList, function (idx, item) {
         //console.log(item);
         output += `
-        <div class="section" style="background-image:url(${item.bg})">
-          <div class="info">
-            <p class="category" data-splitting>${item.category}</p>
-            <h2 class="title" data-splitting>${item.title}</h2>
-            <p class="depth" data-splitting><strong>${item.depth}</strong> mm</p>
-            <p class="price" data-splitting>CHF ${item.price}</p>
+          <div class="section" style="background-image:url(${item.bg})">
+            <div class="info">
+              <p class="category" data-splitting>${item.category}</p>
+              <h2 class="title" data-splitting>${item.title}</h2>
+              <p class="depth" data-splitting><strong>${item.depth}</strong> mm</p>
+              <p class="price" data-splitting>CHF ${item.price}</p>
+            </div>
           </div>
-        </div>
-        `;
+          `;
       });
       main.html(output);
       Splitting();
-      console.log(fp);
-      if (fp !== null) {
+      charMotion(`.section:nth-child(1) .char`);
+      //      $.fn.fullpage.destroy("all");
+      if (fullPage !== null) {
         $.fn.fullpage.destroy("all");
       }
       $("#main").fullpage({
         scrollBar: true,
         onLeave: function (origin, destination, direction) {
           charMotion(`.section:nth-child(${destination.index + 1}) .char`);
-          $(".pagination").text(`${destination.index + 1} / ${total}`);
         },
       });
-      fp = $.fn.fullpage;
-      charMotion(".section:nth-child(1) .char");
-      console.log(fp);
-      $(".pagination").text(`1 / ${total}`);
+      fullPage = $.fn.fullpage;
+      console.log(fullPage);
     },
   });
 }
 
-//두번 사용하니까 펑션 처리...
 function charMotion(char) {
   gsap.from(char, {
     y: -200,
@@ -60,25 +54,49 @@ function charMotion(char) {
     },
   });
 }
+
 // $.get("../data/bigbang.json").done(function (res) {
 //   console.log(res);
 // });
 
-loadJson("../data/bigbang.json");
-particlesJS.load("bgParticle", "../data/particlesjs-config.json");
+// loadJson("../data/bigbang.json");
+// particlesJS.load("bgParticle", "../data/particlesjs-config.json");
 
-$("#gnb li a").on("click", function (e) {
+// $("#gnb li a").on("click", function (e) {
+//   e.preventDefault();
+//   if ($(this).hasClass("selected")) return;
+//   $(this).addClass("selected").parent().siblings("li").find("a").removeClass("selected");
+//   const jsonFile = $(this).data("json");
+//   loadJson(jsonFile);
+// });
+
+// function addZero(num) {
+//   if (num < 10) {
+//     return "0" + num;
+//   } else {
+//     return "" + num;
+//   }
+// }
+
+//1번째 배열....
+/*
+const jsonFileList = ["../data/bigbang.json", "../data/classic.json"];
+const gnbList = $("#gnb li");
+gnbList.on("click", function (e) {
   e.preventDefault();
-  if ($(this).hasClass("selected")) return;
-  $(this).addClass("selected").parent().siblings("li").find("a").removeClass("selected");
+  const idx = $(this).index();
+  console.log(idx);
+  loadJson(jsonFileList[0]);
+});
+*/
+const gnbList = $("#gnb li");
+gnbList.on("click", function (e) {
+  e.preventDefault();
   const jsonFile = $(this).data("json");
+  //console.log(jsonFile);
+  if ($(this).hasClass("selected")) {
+    return;
+  }
+  $(this).addClass("selected").siblings("li").removeClass("selected");
   loadJson(jsonFile);
 });
-
-function addZero(num) {
-  if (num < 10) {
-    return "0" + num;
-  } else {
-    return "" + num;
-  }
-}
